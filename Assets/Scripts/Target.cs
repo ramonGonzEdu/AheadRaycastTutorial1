446f0b1b0316c4b9b314601b2f256256792e749f
Ramon Gonzalez Fernandez <recdeclec.ceo@gmail.com>
2021-02-14 16:58:19 -0500

    add stuff

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+
+	public float health = 50f;
+	public GameObject explosion;
+
+	private void Start()
+	{
+
+	}
+
+	private void Update()
+	{
+
+	}
+
+	public void TakeDamage(float damage)
+	{
+		health -= damage;
+		if (health <= 0) { Instantiate(explosion, transform.position, transform.rotation); Destroy(gameObject); }
+	}
+}
